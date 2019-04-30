426df42814c327519017a24f2fa2ad71f2c0a9f1
pablomartinezs91 <50110886+pablomartinezs91@users.noreply.github.com>
2019-04-30 14:52:12 -0400

    update find-collection

--- middlewares/records/find-collection.js
+++ middlewares/records/find-collection.js
@@ -1,13 +1,42 @@
+var Client = require("../../models/record").Client;
+
 module.exports = function(req, res, next){
 	switch(res.locals.user.permission){
 		case "Admin":
-			
+			Client.find({})
+				.populate("records").exec(function(err, client){
+					if(!err){
+						res.locals.client = client;
+						next();
+					}
+					else{
+						res.redirect("/session");
+					}
+				})
 			break;
 		case "Branch":
-			
+			Client.find({})
+			.populate("records").exec(function(err, client){
+				if(!err){
+					res.locals.client = client;
+					next();
+				}
+				else{
+					res.redirect("/session");
+				}
+			})
 			break;
 		case "Resource":
-			
+			Client.find({})
+			.populate("records").exec(function(err, client){
+				if(!err){
+					res.locals.client = client;
+					next();
+				}
+				else{
+					res.redirect("/session");
+				}
+			})
 			break;
 		default:
 			console.log("Acceso no autorizado");
